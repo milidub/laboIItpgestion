462781f57d1360163291e4f212c1eafcbd7c37e1
FrancRamirez <francisco76.ef@gmail.com>
2024-06-21 14:11:02 -0300

    Update clsLogin.h

--- clsLogin.h
+++ clsLogin.h
@@ -1,66 +1,36 @@
-#ifndef CLSLOGIN_H_INCLUDED
-#define CLSLOGIN_H_INCLUDED
-
 class Login{
-private:
-    char *usuario;
-    char *contrasena;
-    char *nombre;
+    private:
+        char usuario[40];
+        char contrasena[40];
+    public:
+        void Cargar(){
+            cout<<"USUARIO: ";
+            cargarCadena(usuario,40);
+            cout<<"CONTRASENA: ";
+            cargarCadena(contrasena,40);
+        }
+        char *getUsuario(){return usuario;}
+        char *getContrasena(){return contrasena;}
 
-public:
-    void Logear(){
-        FILE *p=fopen("UsuarioContrasena.dat","rb");
-        if(p==NULL)return;
-        cout<<"NOMBRE: ";
-        cin>>nombre;
-        while(fread(this, sizeof this, 1, p)){
-            if(strcmp(nombre, usuario)==0){
-                break;
+        bool operator==(Login obj){
+            if(strcmp(obj.getUsuario(), usuario)==0){
+                if(strcmp(obj.getContrasena(), contrasena)==0){
+                    return true;
+                }
+                return false;
             }
+            return false;
         }
-        cout<<"CONTRASENA: ";
-        cin>>nombre;
-        while(fread(this, sizeof this, 1, p)){
-            if(strcmp(nombre, contrasena)==0){
-                break;
-            }
-        }
-        fclose(p);
-    }
+
 };
 
-class Registro{
-private:
-    char *usuario;
-    char *contrasena;
-    char *n;
-public:
-    void Registrar(){
-        cout<<"NOMBRE DE USUUARIO: ";
-        cin>>n;
-        usuario = new char [strlen(n)];
-        strcpy(usuario, n);
-        cout<<"CONTRASENA: ";
-        cin>>n;
-        contrasena = new char [strlen(n)];
-        strcpy(contrasena, n);
-        cout<<"CONTRASENA OTRA VEZ: ";
-        cin>>n;
-        if(strcmp(n, contrasena)==0){
-            cout<<"Ingresado"<<endl;
-        }else{
-            cout<<"No ingresado"<<endl;
-        }
-        system("pause");
-        FILE *p=fopen("UsuarioContrasena.dat","ab");
-        if(p==NULL)return;
-        fwrite(this, sizeof this, 1, p);
-        fclose(p);
-
-        delete[] usuario;
-        delete[] contrasena;
-    }
-};
+bool grabarLogin(Login l){
+    FILE *p=fopen("Login.dat","ab");
+    if(p==NULL)return false;
+    fwrite(&l, sizeof l, 1, p);
+    fclose(p);
+    return true;
+}
 
 
 
