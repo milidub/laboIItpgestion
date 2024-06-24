82ab8698ebd34d909e472b9e86850e86aa11c0d5
FrancRamirez <francisco76.ef@gmail.com>
2024-06-23 21:49:06 -0300

    Update clsTurnos.h

--- clsTurnos.h
+++ clsTurnos.h
@@ -1,66 +1,33 @@
 #ifndef CLSTURNOS_H_INCLUDED
 #define CLSTURNOS_H_INCLUDED
 
-class Turnos{
-private:
-    int dni;
-    int matricula;
-    char *observaciones;
-    Fecha fecha;
-    bool estado;
-public:
-    void Mostrar(){
-        cout<<"DNI DEL PACIENTE: "<<dni<<endl;
-        cout<<"MATRICULA DEL PACIENTE QUE ATENDERA: "<<matricula<<endl;
-        cout<<"FECHA PARA EL TURNO: ";
-        fecha.Mostrar();
-        fecha.MostrarHoraYMinuto();
-    }
-    void CargarFecha(){
-        fecha.CargarFechaYHora();
-    }
-    int getDniPaciente(){return dni;}
-    int getMatriculaProfesional(){return matricula;}
-
-    void setDniPaciente(int d){dni=d;}
-    void setMatriculaProfesional(int m){matricula=m;}
-    void setFecha(Fecha f){fecha=f;}
-    void setEstado(bool e){estado=e;}
-};
-
-bool grabarTurno(Turnos obj){
-    FILE *p = fopen("turnos.dat","ab");
-    if(p==NULL)return false;
-    fwrite(&obj, sizeof obj, 1, p);
-    fclose(p);
-    return true;
-}
-
-void listarTurnos(){
-    Turnos obj;
-    FILE *p = fopen("turnos.dat","ab");
-    if(p==NULL)return;
-    while(fread(&obj, sizeof obj, 1, p)){
-        obj.Mostrar();
-        cout<<endl;
-    }
-    fclose(p);
-}
-
-void buscarYleerTurnoPaciente(int dni){
-    Turnos obj;
-    FILE *p=fopen("turnos.dat","rb");
-    if(p==NULL){return;}
-    while(fread(&obj, sizeof obj, 1, p)){
-        if(dni==obj.getDniPaciente()){
-            obj.Mostrar();
-            fclose(p);
-            cout<<endl;
-            return;
+class Turno{
+    private:
+        int matriculaProfesional;
+        int dniPaciente;
+        char observaciones[400];
+        Fecha fecha;
+        int hora, minuto;
+        bool estado;
+    public:
+        bool Mostrar(){
+            cout<<"DNI DEL PACIENTE: "<<dniPaciente<<endl;
+            cout<<"MATRICULA PROFESIONAL: "<<matriculaProfesional<<endl;
+            cout<<"OBSERVACIONES: "<<observaciones<<endl;
+            cout<<"FECHA: ";
+            fecha.Mostrar();
+            cout<<"HORA Y MINUTO: "<<hora<<" : "<<minuto<<endl;
+            return estado;
         }
-    }
-    cout<<"TURNO INEXISTENTE"<<endl;
-    fclose(p);
-}
+        void setDniPaciente(int dni){dniPaciente = dni;}
+        void setMatriculaProfesional(int matricula){matriculaProfesional = matricula;}
+        void setObservaciones(const char *o){strcpy(observaciones, o);}
+        void setFecha(Fecha f){fecha = f;}
+        void setHora(int h){hora = h;}
+        void setMinuto(int m){minuto = m;}
+        void setEstado(bool e){estado = e;}
 
+        bool getEstado(){return estado;}
+        int getDniPaciente(){return dniPaciente;}
+};
 #endif // CLSTURNOS_H_INCLUDED
