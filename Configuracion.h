919585e90f4d5a44ace0d43a21c7e307ed02be01
Florencia Palacio <137527381+florrpalacio@users.noreply.github.com>
2024-06-26 22:22:52 -0300

    Update Configuracion.h

--- Configuracion.h
+++ Configuracion.h
@@ -4,19 +4,27 @@
 void copiaSeguridad();
 void recibrCopiaSeguridad();
 void registrarse();
+void listarRegistros();
 
 void menuConfiguracion(){
     int num;
     while(true){
-        cout<<" ========================="<<endl;
-        cout<<"|      CONFIGURACION     |"<<endl;
-        cout<<" ========================="<<endl;
-        cout<<"1. COPIA DE SEGURIDAD DE TODAS LAS CLASES"<<endl;
-        cout<<"2. RECIBIR COPIA DE SEGURIDAD DE TODAS LAS CLASES"<<endl;
-        cout<<"3. REGISTRARSE           "<<endl;
-        cout<<"========================="<<endl;
-        cout<<"0. SALIR"<<endl;
-        cout<<"INGRESE OPCION: ";
+        rlutil::setColor(rlutil::MAGENTA);
+        interfaz();
+        rlutil::setColor(rlutil::YELLOW);
+        rlutil::locate(53, 7);
+        cout<<"CONFIGURACION"<<endl;
+        rlutil::locate(50, 10);
+        cout<<"1. COPIA DE SEGUIRDAD GENERAL"<<endl;
+        rlutil::locate(50, 12);
+        cout<<"2. RECIBIR COPIA DE SEGURIDAD GENERAL"<<endl;
+        rlutil::locate(50, 14);
+        cout<<"3. REGISTRARSE"<<endl;
+        rlutil::locate(50, 16);
+        cout<<"4. LISTAR REGISTROS"<<endl;
+        rlutil::locate(50, 20);
+        cout<<"INGRESE OPCION: "<<endl;
+        rlutil::locate(66, 20);
         cin>>num;
         system("cls");
         switch(num){
@@ -26,12 +34,15 @@ void menuConfiguracion(){
                 break;
             case 3: registrarse();
                 break;
+            case 4: listarRegistros();
+                break;
             case 0:
                 return;
             default:
                 cout<<"ERROR"<<endl;
                 break;
         }
+        rlutil::locate(0, 256);
         system("pause");
         system("cls");
     }
@@ -155,33 +166,85 @@ void copiaSeguridad(){
     copiaSeguridadPacientes();
     copiaSeguridadProfesional();
     copiaSeguridadTurnos();
-    cout<<"SE HA GUARDADO A LA COPIA DE SEGURIDAD"<<endl;
+    rlutil::locate(50, 7);
+    cout<<"COPIADO CON EXITO"<<endl;
 }
 
 void recibrCopiaSeguridad(){
     recibirCopiaSeguridadPacientes();
     recibirCopiaSeguridadProfesional();
     recibirCopiaSeguridadTurnos();
-    cout<<"SE HA RECIBIDO LA COPIA DE SEGURIDAD CON EXITO"<<endl;
+    rlutil::locate(50, 7);
+    cout<<"PEGADO CON EXITO"<<endl;
 }
 
 void registrarse(){
     Login obj, obj2;
-    bool bandera=false;
+    char opc;
+
     obj.Cargar();
+
     FILE *p=fopen("Login.dat","rb");
     if(p==NULL)return;
     while(fread(&obj2, sizeof obj2, 1, p)){
         if(obj==obj2){
-            cout<<"YA EXISTE UN NOMBRE DE USUARIO Y CONTRASENA"<<endl;
+            system("cls");
+            rlutil::setColor(rlutil::RED);
+            interfazLogin();
+            rlutil::locate(50, 12);
+            cout<<"YA EXISTE UN NOMBRE DE USUARIO"<<endl;
+            rlutil::locate(50, 13);
+            cout<<" Y/O CONTRASENA"<<endl;
             return;
         }
     }
-    if(grabarLogin(obj)){
-        cout<<"REGISTRADO CON EXITO"<<endl;
+    rlutil::locate(50, 12);
+    cout<<"DESEA REGISTRARSE?   "<<endl;
+    rlutil::locate(50, 13);
+    cout<<"1: SI"<<"    "<<"2: NO       "<<endl;
+    rlutil::locate(50, 14);
+    cout<<"                          "<<endl;
+    while(true){
+        rlutil::setColor(rlutil::WHITE);
+        rlutil::locate(50, 14);
+        cin>>opc;
+        switch(opc){
+        case 1:
+            if(grabarLogin(obj)){
+                rlutil::setColor(rlutil::GREEN);
+                interfazLogin();
+                rlutil::locate(50, 13);
+                cout<<"REGISTRADO CON EXITO"<<endl;
+            }else{
+                rlutil::setColor(rlutil::RED);
+                interfazLogin();
+                rlutil::locate(50, 13);
+                cout<<"ERROR AL REGISTRARSE"<<endl;
+            }
+            return;
+        break;
+
+        case 2: return;
+            break;
+
+        default:
+            rlutil::locate(50, 13);
+            rlutil::setColor(rlutil::RED);
+            cout<<"INGRESE NUEVAMENTE LA OPCION"<<endl;
+            break;
+        }
     }
-    else{
-        cout<<"ERROR AL REGISTRARSE"<<endl;
+}
+
+void listarRegistros(){
+    Login obj2;
+
+    FILE *p=fopen("Login.dat","rb");
+    if(p==NULL)return;
+    while(fread(&obj2, sizeof obj2, 1, p)){
+        cout<<obj2.getUsuario()<<endl;
+        cout<<obj2.getContrasena()<<endl;
+        cout<<endl;
     }
 }
 
