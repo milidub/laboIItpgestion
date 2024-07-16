be0e7d10042a13a92ffa62cf12d7d1a6680db3e4
FrancRamirez <francisco76.ef@gmail.com>
2024-07-16 12:01:22 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,73 +7,20 @@ using namespace std;
 
 #include "rlutil.h"
 #include "CargarCadena.h"
-#include "clsFecha.h"
 #include "interfaz.h"
-#include "clsPacientes.h"
-#include "clsArchivoPacientes.h"
+#include "clsFecha.h"
+#include "clsPaciente.h"
+#include "clsArchivoPaciente.h"
 #include "menuPaciente.h"
 #include "clsProfesional.h"
 #include "clsArchivoProfesional.h"
 #include "menuProfesional.h"
-#include "clsTurnos.h"
-#include "clsArchivoTurnos.h"
+#include "clsTurno.h"
+#include "clsArchivoTurno.h"
 #include "menuTurno.h"
 #include "clsLogin.h"
 #include "Configuracion.h"
-
-///PORTAL DE TURNOS
-
-///CLASES PRINCIALES A USAR:
-///PACIENTE
-///PROFESIONAL
-///TURNOS
-
-///ARCHIVOS A USAR:
-///ARCHIVOPACIENTE (pacientes.dat)
-///ARCHIVOPROFESIONAL (profesional.dat)
-///ARCHIVOTURNO(turnos.dat)
-
-///CLASES SECUNDARIAS:
-///FECHA
-///LOGIN (nombre y contraseña)
-
-///CAMPOS DE LAS CLASES PRINCIPALES:
-///PACIENTE:
-/**
-nombre (char)
-apellido (char)
-fecha de nacimiento (FECHA)
-dni (int)
-obra social (int)
-direccion (char)
-telefono (int)
-email (char)
-estado (bool)
-*/
-
-///PROFESIONAL:
-/**
-nombre (char)
-apellido (char)
-matricula (int)
-fecha de ingreso (FECHA)
-especialidad (int)
-direccion (char)
-telefono (int)
-email (char)
-estado (bool)
-*/
-
-///TURNO:
-/**
-matriculaProfesional [Recibe el parametro de la matricula Profesional]
-dniPaciente [recibe el parametro del dni Paciente]
-observaciones (char) [escribe las observaciones]
-fecha (FECHA) [escribe y recibe el parametro de fecha]
-hora (int) [recibe el parametro de hora]
-minuto (int) [recibe el parametro de minuto]
-estado (bool)
-*/
+#include "Reportes.h"
 
 
 int main()
@@ -84,6 +31,11 @@ int main()
     while(true){
         rlutil::setColor(rlutil::GREY);
         interfaz();
+        rlutil::setColor(rlutil::WHITE);
+        FechaActual fa;
+        rlutil::locate(53, 6);
+        cout<<"FECHA: ";
+        fa.Mostrar();
         rlutil::setColor(rlutil::YELLOW);
         rlutil::locate(53, 7);
         cout<<"MENU PRINCIPAL"<<endl;
@@ -96,10 +48,12 @@ int main()
         rlutil::locate(50, 16);
         cout<<"4. CONFIGURACION"<<endl;
         rlutil::locate(50, 18);
-        cout<<"0. SALIR"<<endl;
+        cout<<"5. REPORTES"<<endl;
         rlutil::locate(50, 20);
+        cout<<"0. SALIR"<<endl;
+        rlutil::locate(50, 22);
         cout<<"INGRESE OPCION: "<<endl;
-        rlutil::locate(66, 20);
+        rlutil::locate(66, 22);
         cin>>num;
         system("cls");
         switch(num){
@@ -110,6 +64,8 @@ int main()
             case 3: menuTurno();
                 break;
             case 4: menuConfiguracion();
+                break;
+            case 5: MenuReportes();
                 break;
             case 0:
                 return 0;
@@ -122,4 +78,3 @@ int main()
     }
     return 0;
 }
-
